4a00f4a42d645e8773d5bc939c950557988d4476
S Yoon <sohizzhang@gmail.com>
2019-06-06 13:27:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def bark
-    puts "Woof!"
+  def name=(dog_name)
+    this_dogs_name = dog_name
   end
-
-  def sit
-    puts "The Dog is sitting"
+ 
+  def name
+    this_dogs_name
   end
 end
